3efebdac2da4c33b1cc55524479b52ca86c012dd
hammad saeed <hammad.saeed2410@gmail.com>
2020-11-23 15:20:48 +0500

    added https

--- src/configs/server.js
+++ src/configs/server.js
@@ -2,6 +2,9 @@ var express = require('express')
 var app = express()
 var multer = require('multer');
 var upload = multer();
+var https = require('https');
+var fs = require('fs');
+var path = require('path');
 
 
 app.use(express.static('public'));
@@ -10,8 +13,22 @@ app.use(upload.array());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.urlencoded({ extended: false }));
 
+var privateKey = fs.readFileSync(path.resolve(__dirname, "domain.key"), 'utf8');
+var certificate = fs.readFileSync(path.resolve(__dirname, "domain.crt"), 'utf8');
+
 const port = process.env.PORT || 3000;
 
+const option = {
+    cert: certificate,
+    key: privateKey
+}
+
+//console.log(option.cert)
+
+var server = https.createServer(option, app);
+
+
+server.listen(3001, () => console.log(`Listening on port 3001`))
 app.listen(port, () => console.log(`Listering on port ${port}`))
 
 
