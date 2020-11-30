06654836c648c23a2e5cecfef47deb98dd220c49
hammad saeed <hammad.saeed2410@gmail.com>
2020-11-30 16:45:13 +0500

    admin new apis added

--- src/routes/admin.js
+++ src/routes/admin.js
@@ -127,7 +127,7 @@ adminRoute.post("/getallusers", (req, res) => {
 
 adminRoute.get('/getUsers', (req, res) => {
 
-    connection.query("SELECT b.id as buyer, d.id as distributor , r.id as representative, b.email as email ,b.name as name, b.status FROM public.buyers as b LEFT JOIN public.distributors as d ON b.id = d.buyer_id LEFT JOIN public.representatives as r ON b.id = r.buyer_id", (error, result) => {
+    connection.query("SELECT b.id as buyer, d.id as distributor , r.id as representative, b.email as email ,b.name as name, b.status as user_status, d.status as distributor_status, r.status as representative_status FROM public.buyers as b LEFT JOIN public.distributors as d ON b.id = d.buyer_id LEFT JOIN public.representatives as r ON b.id = r.buyer_id", (error, result) => {
         if (error) {
             res.send({
                 success: false,
@@ -135,16 +135,6 @@ adminRoute.get('/getUsers', (req, res) => {
             })
         }
         else {
-
-            console.log("Result: ", result.rows)
-
-            // var users = result.rows.map((user) => {
-            //     return ({
-            //         buyer: JSON.parse(user.buyer),
-
-            //     })
-            // })
-
             res.send({
                 success: true,
                 users: result.rows
@@ -152,6 +142,132 @@ adminRoute.get('/getUsers', (req, res) => {
         }
     })
 
+})
+
+adminRoute.post('/getDistributor', (req, res) => {
+
+    var id = req.body.id
+
+    connection.query("SELECT * FROM distributors WHERE id = $1", [id], (error, result) => {
+        if (error) {
+            res.send({
+                success: false,
+                message: error.message
+            })
+        }
+        else {
+            connection.query('SELECT * FROM brands WHERE distributor_id = $1', [id], (error1, result1) => {
+                if (error1) {
+                    res.send({
+                        success: false,
+                        message: error1.message
+                    })
+                }
+                else {
+                    connection.query('SELECT * FROM certificates WHERE distributer_id = $1', [id], (error2, result2) => {
+                        if (error2) {
+                            res.send({
+                                success: false,
+                                message: error2.message
+                            })
+                        }
+                        else {
+                            res.send({
+                                success: true,
+                                distributor: result.rows[0],
+                                brands: result1.rows,
+                                certificates: result2.rows
+                            })
+                        }
+                    })
+                }
+            })
+
+        }
+    })
+
+})
+
+adminRoute.post('/getRepresentative', (req, res) => {
+
+    var id = req.body.id
+
+    connection.query("SELECT * FROM representatives WHERE id = $1", [id], (error, result) => {
+        if (error) {
+            res.send({
+                success: false,
+                message: error.message
+            })
+        }
+        else {
+            res.send({
+                success: true,
+                message: "Representative fetched",
+                representative: result.rows[0]
+            })
+        }
+    })
+
+})
+
+adminRoute.post('/setDistributorStatus', (req, res) => {
+    var id = req.body.id
+    var status = req.body.status
+
+    connection.query("UPDATE distributors SET status = $1 WHERE id = $2", [status, id], (error, result) => {
+        if (error) {
+            res.send({
+                success: false,
+                message: error.message
+            })
+        }
+        else {
+            res.send({
+                success: true,
+                message: "Status updated"
+            })
+        }
+    })
+})
+
+adminRoute.post('/setRepresentativeStatus', (req, res) => {
+    var id = req.body.id
+    var status = req.body.status
+
+    connection.query("UPDATE representatives SET status = $1 WHERE id = $2", [status, id], (error, result) => {
+        if (error) {
+            res.send({
+                success: false,
+                message: error.message
+            })
+        }
+        else {
+            res.send({
+                success: true,
+                message: "Status updated"
+            })
+        }
+    })
+})
+
+adminRoute.post('/setBuyerStatus', (req, res) => {
+    var id = req.body.id
+    var status = req.body.status
+
+    connection.query("UPDATE Buyers SET status = $1 WHERE id = $2", [status, id], (error, result) => {
+        if (error) {
+            res.send({
+                success: false,
+                message: error.message
+            })
+        }
+        else {
+            res.send({
+                success: true,
+                message: "Status updated"
+            })
+        }
+    })
 })
 
 module.exports = adminRoute
